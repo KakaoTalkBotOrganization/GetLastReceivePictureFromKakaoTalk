fe9f62db143e61fe528821bb5f043839a557554c
SungBin <sungbin.dev@gmail.com>
2020-08-18 14:57:00 +0900

    Update PicturePathManager.js
    apply the `Airbnb` code style.

--- PicturePathManager.js
+++ PicturePathManager.js
@@ -2,9 +2,9 @@ const PICTURE_PATH = "/sdcard/Android/data/com.kakao.talk/contents/Mg==";
 
 const getLastPictureAsBase64 = () => {
     try {
-        const getLatestModified = array => array.reduce((a, b) => (a.lastModified() < b.lastModified()) ? b : a);
+        const getLatestModified = (array) => array.reduce((a, b) => (a.lastModified() < b.lastModified()) ? b : a);
         const level1 = getLatestModified(new java.io.File(PICTURE_PATH).listFiles());
-        const level2 = getLatestModified(level1.listFiles().filter(f => f.list().length !== 0));
+        const level2 = getLatestModified(level1.listFiles().filter((f) => f.list().length !== 0));
         const file = getLatestModified(level2.listFiles());
         const bm = android.graphics.BitmapFactory.decodeFile(file.toString());
         const baos = new java.io.ByteArrayOutputStream();
