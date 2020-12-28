3692544d9fbbed3b7df3873637fc7f730bf9a209
Sungbin Ji <sungbin.dev@gmail.com>
2020-12-28 23:11:08 +0900

    add annotation

--- PicturePathManager.java
+++ PicturePathManager.java
@@ -3,32 +3,31 @@ import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.util.Base64;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Objects;
 
 import javax.annotation.Nullable;
 
-public class PicturePathManager {
+public class PictureManager {
+
     private static final String sdcard = Environment.getExternalStorageDirectory().getAbsolutePath();
-    private static final String PICTURE_PATH = sdcard + "/Android/data/com.kakao.talk/contents/Mg==";
+    private static final String KAKAOTALK_PICTURE_PATH = sdcard + "/Android/data/com.kakao.talk/contents/Mg==";
 
-    private static class ModifiedDate implements Comparator<File> {
-        public int compare(File f1, File f2) {
-            return Long.compare(f2.lastModified(), f1.lastModified());
-        }
-    }
-
-    public static String getLastPictureFolderPath(){
-        File file = new File(PICTURE_PATH);
+    @NotNull
+    public static String getKakaoTalkLastPictureFolderPath() {
+        File file = new File(KAKAOTALK_PICTURE_PATH);
         File[] list = file.listFiles();
         Arrays.sort(Objects.requireNonNull(list), new ModifiedDate());
         return list[0].toString();
     }
 
-    public static File[] getLastPictureFilePathFromFoldPath(String path){
+    public static File[] getKakaoTalkLastPictureFilePathFromFoldPath(String path) {
         File file = new File(path);
         File[] list = file.listFiles();
         Arrays.sort(Objects.requireNonNull(list), new ModifiedDate());
@@ -36,13 +35,13 @@ public class PicturePathManager {
     }
 
     @Nullable
-    public static String getLastPicture(){
-        try{
-            File[] path = getLastPictureFilePathFromFoldPath(getLastPictureFolderPath());
+    public static String getKakaoTalkLastPictureBase64() {
+        try {
+            File[] path = getKakaoTalkLastPictureFilePathFromFoldPath(getKakaoTalkLastPictureFolderPath());
             for (File value : path) {
                 File file = new File(value.toString());
-                if (Objects.requireNonNull(file.listFiles()).length > 0)  {
-                    String picture = getLastPictureFilePathFromFoldPath(file.getPath())[0].toString();
+                if (Objects.requireNonNull(file.listFiles()).length > 0) {
+                    String picture = getKakaoTalkLastPictureFilePathFromFoldPath(file.getPath())[0].toString();
                     Bitmap bm = BitmapFactory.decodeFile(picture);
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
                     bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
@@ -51,9 +50,14 @@ public class PicturePathManager {
                 }
             }
             return null;
-        }
-        catch(Exception ignored){
+        } catch (Exception ignored) {
             return null;
+        }
+    }
+
+    private static class ModifiedDate implements Comparator<File> {
+        public int compare(@NotNull File f1, @NotNull File f2) {
+            return Long.compare(f2.lastModified(), f1.lastModified());
         }
     }
 }
